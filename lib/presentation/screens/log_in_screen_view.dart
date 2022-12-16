63548f3f2eee025bc591229fdaf05dd8bac95801
alemmiwi <yellowfirehydrant@icloud.com>
2022-12-16 23:54:57 +0300

    add padding to app scaffold and fix en translate

--- lib/presentation/screens/log_in_screen_view.dart
+++ lib/presentation/screens/log_in_screen_view.dart
@@ -22,65 +22,58 @@ class _HomeScreenState extends State<LogInScreenView> {
   @override
   Widget build(BuildContext context) {
     return AppScaffold(
-      child: Padding(
-        padding: const EdgeInsets.only(left: 18, right: 18),
-        child: Column(
-          children: [
-            AppOffsetBox.heightLargeBox,
-            AppText(
-              'whats_your_number',
-              alignment: Alignment.topLeft,
-              style: AppTextStyle.largeTitleSemibold.copyWith(
-                  color: AppColorSchemeHelper.getColorScheme(context).primary),
-            ),
-            AppOffsetBox.heightMediumBox,
-            AppPhoneTextfield(),
-            Spacer(),
-            Padding(
-              padding: const EdgeInsets.only(
-                left: AppPaddingGrade.regularPadding,
-                right: AppPaddingGrade.regularPadding,
-              ),
-              child: RichText(
-                textAlign: TextAlign.center,
-                text: TextSpan(
-                  children: <TextSpan>[
-                    TextSpan(
-                      text: 'agreeing_text'.tr(),
-                      style: AppTextStyle.captionMedium.copyWith(
-                          color: AppColorSchemeHelper.getColorScheme(context)
-                              .tertiary),
-                    ),
-                    TextSpan(
-                      text: 'terms_of_service'.tr(),
-                      style: AppTextStyle.captionMedium.copyWith(
-                          color: AppColorSchemeHelper.getColorScheme(context)
-                              .primary),
-                    ),
-                    TextSpan(
-                      text: 'and'.tr(),
-                      style: AppTextStyle.captionMedium.copyWith(
-                          color: AppColorSchemeHelper.getColorScheme(context)
-                              .tertiary),
-                    ),
-                    TextSpan(
-                      text: 'privacy_policy'.tr(),
-                      style: AppTextStyle.captionMedium.copyWith(
-                          color: AppColorSchemeHelper.getColorScheme(context)
-                              .primary),
-                    ),
-                  ],
+      paddingLeft: AppPaddingGrade.regularPadding,
+      paddingRight: AppPaddingGrade.regularPadding,
+      child: Column(
+        children: [
+          AppOffsetBox.heightLargeBox,
+          AppText(
+            'whats_your_number',
+            alignment: Alignment.topLeft,
+            style: AppTextStyle.largeTitleSemibold.copyWith(
+                color: AppColorSchemeHelper.getColorScheme(context).primary),
+          ),
+          AppOffsetBox.heightMediumBox,
+          AppPhoneTextfield(),
+          Spacer(),
+          RichText(
+            textAlign: TextAlign.center,
+            text: TextSpan(
+              children: <TextSpan>[
+                TextSpan(
+                  text: 'agreeing_text'.tr(),
+                  style: AppTextStyle.captionMedium.copyWith(
+                      color: AppColorSchemeHelper.getColorScheme(context)
+                          .tertiary),
                 ),
-              ),
+                TextSpan(
+                  text: 'terms_of_service'.tr(),
+                  style: AppTextStyle.captionMedium.copyWith(
+                      color:
+                          AppColorSchemeHelper.getColorScheme(context).primary),
+                ),
+                TextSpan(
+                  text: 'and'.tr(),
+                  style: AppTextStyle.captionMedium.copyWith(
+                      color: AppColorSchemeHelper.getColorScheme(context)
+                          .tertiary),
+                ),
+                TextSpan(
+                  text: 'privacy_policy'.tr(),
+                  style: AppTextStyle.captionMedium.copyWith(
+                      color:
+                          AppColorSchemeHelper.getColorScheme(context).primary),
+                ),
+              ],
             ),
-            AppOffsetBox.heightRegularBox,
-            AppFilledTextButton(
-              title: 'continue',
-              onTap: () {},
-            ),
-            AppOffsetBox.heightRegularBox,
-          ],
-        ),
+          ),
+          AppOffsetBox.heightRegularBox,
+          AppFilledTextButton(
+            title: 'continue',
+            onTap: () {},
+          ),
+          AppOffsetBox.heightRegularBox,
+        ],
       ),
     );
   }
--- lib/presentation/widgets/app_scaffold.dart
+++ lib/presentation/widgets/app_scaffold.dart
@@ -7,10 +7,14 @@ class AppScaffold extends StatefulWidget {
     Key? key,
     required this.child,
     this.color,
+    this.paddingLeft,
+    this.paddingRight,
   }) : super(key: key);
 
   final Widget child;
   final Color? color;
+  final double? paddingLeft;
+  final double? paddingRight;
 
   @override
   State<AppScaffold> createState() => _AppScaffoldState();
@@ -28,6 +32,8 @@ class _AppScaffoldState extends State<AppScaffold> {
           padding: EdgeInsets.only(
             top: AppMediaQueryPaddingHelper.getPadding(context).top,
             bottom: AppMediaQueryPaddingHelper.getPadding(context).bottom,
+            left: widget.paddingLeft ?? 0.0,
+            right: widget.paddingRight ?? 0.0,
           ),
           child: widget.child,
         ),
